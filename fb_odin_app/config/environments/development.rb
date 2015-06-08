0e4a38a4d81363b17ba4b357f85be805cb50e120
Alex Hardt <hardt.alex@gmail.com>
2015-06-08 17:43:24 -0500

    read below, trying to set up foundations correctly:
    you were trying to set up heroku pg databse, then moved to creating static pages as a home page for your app, then you would create the devise views, but it looks like you might need some kind of foundational database before rails will even start to work with you. To begin tmrw: try and fire up server and follow errors

--- fb_odin_app/config/environments/development.rb
+++ fb_odin_app/config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  #1. Ensure you have defined default url options in your environments files. Here is an example of default_url_options appropriate for a development environment in config/environments/development.rb:
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
