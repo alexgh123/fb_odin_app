79d43eb5939d9838bbae167ae72da92d74f8f39d
Alex Hardt <hardt.alex@gmail.com>
2015-06-09 15:03:13 -0500

    if a user has posts, they are shown on his her posts page, its not pretty, but works, time to clean up now

--- fb_odin_app/app/controllers/users_controller.rb
+++ fb_odin_app/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.paginate(page: params[:page])
+    @posts = @user.posts#.paginate(page: params[:page])
   end
 
 end
