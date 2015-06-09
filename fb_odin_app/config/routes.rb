5e2b06d9d66dd1fc6bf9fb8b8e238b69b22b0287
Alex Hardt <hardt.alex@gmail.com>
2015-06-09 14:56:23 -0500

    generalish post crud things work, need to build the partial

--- fb_odin_app/config/routes.rb
+++ fb_odin_app/config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/:id' => 'users#show'
 
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
