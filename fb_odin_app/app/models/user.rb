287f82b84e988566cae9b3cb8fe4be845e850753
Alex Hardt <hardt.alex@gmail.com>
2015-06-09 11:29:32 -0500

    i can create user objects with devise, however, i cant add attributes to them with the stock sign up and update pages

--- fb_odin_app/app/models/user.rb
+++ fb_odin_app/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_save { self.email = email.downcase }
+  validates :first_name, presence: true, length: {maximum: 25}
+  validates :last_name, presence: true, length: {maximum: 25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 35},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+
+  attr_accessor :first_name, :last_name
 end
