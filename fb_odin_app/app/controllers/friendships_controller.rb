1a07bf6c7ddb821b391c1df9ee8e2a036236f2aa
Alex Hardt <hardt.alex@gmail.com>
2015-06-12 09:02:21 -0500

    friendships cant be accessed w/o signing in

--- fb_odin_app/app/controllers/friendships_controller.rb
+++ fb_odin_app/app/controllers/friendships_controller.rb
@@ -1,4 +1,5 @@
 class FriendshipsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_friendship, only: [:show, :edit, :update, :destroy]
 
   # GET /friendships
