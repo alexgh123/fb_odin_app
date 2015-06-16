a5c6cbdcf5e57a6431e88ed0df33083cd7f434e6
Alex Hardt <hardt.alex@gmail.com>
2015-06-16 17:24:22 -0500

    i can create comments on the page!, i just slowed down and stopped panicing, embarasing

--- fb_odin_app/app/controllers/comments_controller.rb
+++ fb_odin_app/app/controllers/comments_controller.rb
@@ -21,5 +21,17 @@ class CommentsController < ApplicationController
     redirect_to :back
   end
 
+def create
+  @comment = Comment.new(comment_params)
+
+
+  @comment.save
+
+  redirect_to root_path
+end
+
+def comment_params
+  params.require(:comment).permit(:content, :user_id, :post_id)
+end
 
 end
