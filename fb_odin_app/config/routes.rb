a53e3eeed2364cad0c092a58356eb578f4141a9a
Alex Hardt <hardt.alex@gmail.com>
2015-06-16 09:10:19 -0500

    basic comment creation works from controller perspective, need to have destroy controller actions too

--- fb_odin_app/config/routes.rb
+++ fb_odin_app/config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   resources :likes, only: [:create, :destroy]
 
+  resources :comments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
