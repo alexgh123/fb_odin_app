963518bd5dc2867fb93ea6b5d491c83588b20f1b
Alex Hardt <hardt.alex@gmail.com>
2015-06-16 08:58:27 -0500

    comment controller started

---
+++ fb_odin_app/app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+
+end
