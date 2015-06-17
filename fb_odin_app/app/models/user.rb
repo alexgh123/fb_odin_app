63341e69f18a9faa556a9ee8f9302a18dfac64b3
Alex Hardt <hardt.alex@gmail.com>
2015-06-17 16:34:21 -0500

    removed old friendship active record code in user model

--- fb_odin_app/app/models/user.rb
+++ fb_odin_app/app/models/user.rb
@@ -13,12 +13,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   #------------- friends! ----------------
-  has_many :friendships
-  has_many :passive_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  has_many :followers, -> { where(friendships: { approved: true}) }, :through => :friendships, :source => :friend
-  has_many :following, -> { where(friendships: { approved: true}) }, :through => :passive_friendships, :source => :user
-  has_many :pending_friends, -> { where(friendships: { approved: false}) }, :through => :friendships, :source => :friend
-  has_many :requested_friendships, -> { where(friendships: { approved: false}) }, :through => :passive_friendships, :source => :user
+
 
   #----------likes!-----------
   has_many :likes
