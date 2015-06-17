b8e5ef082cb0d0b9cf94bc04964d0b29e91a4440
Alex Hardt <hardt.alex@gmail.com>
2015-06-17 08:11:49 -0500

    because you can comment on posts and posts show up in the users show page, there needs to be a comment object ready to go, not sure this is very OO...

--- fb_odin_app/app/controllers/users_controller.rb
+++ fb_odin_app/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts#.paginate(page: params[:page])
+    @comment = Comment.new
   end
 
 end
