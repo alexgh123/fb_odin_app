bb6b170bfd13e421f1ce6d28994f7f5e8bc9d57f
Alex Hardt <hardt.alex@gmail.com>
2015-07-29 15:10:25 -0500

    trying to fix fixtures

--- fb_odin_app/test/controllers/users_controller_test.rb
+++ fb_odin_app/test/controllers/users_controller_test.rb
@@ -4,4 +4,11 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get sign in page" do
+    get :sign_in
+    assert_response :success
+  end
+
+
 end
