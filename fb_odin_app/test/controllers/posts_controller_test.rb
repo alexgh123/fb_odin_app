0f5ed43564217d019224279da1ead27edbba1845
Alex Hardt <hardt.alex@gmail.com>
2015-08-04 15:01:40 -0500

    building tests

--- fb_odin_app/test/controllers/posts_controller_test.rb
+++ fb_odin_app/test/controllers/posts_controller_test.rb
@@ -4,17 +4,29 @@ class PostsControllerTest < ActionController::TestCase
 
   def setup
     @user = users(:user_one)
+    @post = posts(:post_one)
   end
 
-  test 'should redirect from posts index when not logged in' do
+  test 'when not logged in, should redirect from posts index' do
     get :index
     assert_redirected_to new_user_session_path
   end
 
-  test 'should redirect from posts show when not logged in' do
-    get :show, id:1
-    # byebug
+  test 'when not logged in, should redirect from posts show ' do
+    get :show, id:@post
     assert_redirected_to new_user_session_path
+  end
+
+  test 'when logged in, should get home page/posts index' do
+    sign_in @user
+    get :index
+    assert_response :success
+  end
+
+  test 'when logged in, should get posts show ' do
+    sign_in @user
+    get :show, id:@post
+    assert_response :success
   end
 
 
