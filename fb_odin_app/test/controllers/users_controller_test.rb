44cfafba6532ba8b62c2518b14f0b167647a137c
Alex Hardt <hardt.alex@gmail.com>
2015-08-04 08:58:32 -0500

    sign_in method finally works with devise

--- fb_odin_app/test/controllers/users_controller_test.rb
+++ fb_odin_app/test/controllers/users_controller_test.rb
@@ -6,14 +6,27 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:user_one)
   end
 
-  test 'should redirect from index when not logged in' do
+  test 'when not logged in, redirects to login page' do
     get :index
     assert_redirected_to new_user_session_path
   end
 
-  test 'should redirect from users show when not logged in' do
-    get :show, id:@user
+  test 'when not logged in, should redirect from users show ' do
+    get :show, id: @user
     assert_redirected_to new_user_session_path
   end
+
+  test 'when logged in, you do get list of users' do
+    sign_in @user
+    get :index
+    assert_response :success
+  end
+
+  test 'when logged in, you can get a single user' do
+    sign_in @user
+    get :show, id: @user
+    assert_response :success
+  end
+
 
 end
